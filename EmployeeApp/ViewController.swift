1b9e221266f20f0a5261190f80cdd2c1742fde77
Sean Kearney <s.kearney.94@gmail.com>
2019-08-21 02:40:19 +0100

    Initial Commit
    Features Implemented:
    - Fetch employee data from endpoint
    - Parse json data
    - Display employee names in TableView
    
    ToDo
    - Network Activity Indicator
    - Filter Employees
    - Expand on employee data display
    - View more information when tapped

---
+++ EmployeeApp/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  EmployeeApp
+//
+//  Created by Sean Kearney on 20/08/2019.
+//  Copyright © 2019 Sean Kearney. All rights reserved.
+//
+
+import UIKit
+
+struct Employee: Decodable{
+    let id: String
+    let employee_name: String
+    let employee_salary: String
+    let employee_age: String
+}
+
+var employeeList = [Employee]()
+let dispatchGroup = DispatchGroup()
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employeeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeCell", for: indexPath)
+        
+        cell.textLabel!.text = employeeList[indexPath.row].employee_name
+        return cell
+    }
+    
+
+    @IBOutlet weak var TableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dispatchGroup.enter()
+        fetchEmployeeData()
+        TableView.dataSource = self
+        TableView.delegate = self
+        dispatchGroup.notify(queue: .main){
+            self.reloadData()
+        }
+    }
+    
+    func fetchEmployeeData(){
+        let urlString = "http://dummy.restapiexample.com/api/v1/employees"
+        guard let url = URL(string: urlString) else{return}
+       
+        URLSession.shared.dataTask(with: url){(data, response, error) in
+            
+            
+            if let err = error{
+                print(err)
+                return
+            }
+            guard response != nil else{
+                //no response
+                return
+            }
+            guard let data = data else{
+                //empty data
+                return
+            }
+            do{
+                let employees = try JSONDecoder().decode([Employee].self, from: data)
+                print("GOT DATA")
+                employeeList = employees
+                dispatchGroup.leave()
+            } catch{
+                print(error)
+            }
+        }.resume()
+        
+    }
+    
+    func reloadData(){
+        print("REloading")
+        self.TableView.reloadData()
+        print(employeeList)
+    }
+}
+
